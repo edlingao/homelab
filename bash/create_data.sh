32feff85832e2a573540689d6c22af8303db9706
edlingao <sonido244@gmail.com>
2025-07-29 23:01:21 -0600

    usable

---
+++ bash/create_data.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Create a directory for the data
+mkdir -p data/media/{books,movies,music,tv}
+mkdir -p data/media2/{books,movies,music,tv}
+mkdir -p data/torrents/{complete,incomplete,downloads,torrent-files,watch}
+mkdir -p data/usenet/complete/{books,movies,music,tv}
+mkdir -p data/usenet/incomplete/
+
+
