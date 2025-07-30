bb4aa38c8368e351fbf8dc62db179a31eecababf
edlingao <sonido244@gmail.com>
2025-07-30 01:06:00 -0600

    Fixing User Access

--- bash/create_data.sh
+++ bash/create_data.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 
 # Create a directory for the data
-mkdir -p data/media/{books,movies,music,tv};
-mkdir -p data/media2/{books,movies,music,tv};
-mkdir -p data/torrents/{complete,incomplete,downloads,torrent-files,watch};
-mkdir -p data/usenet/complete/{books,movies,music,tv};
-mkdir -p data/usenet/incomplete;
+mkdir -p data/media;
+mkdir -p data/media/books;
+mkdir -p data/media/movies;
+mkdir -p data/media/music;
+mkdir -p data/media/tv;
 
+mkdir -p data/media2;
+mkdir -p data/media2/books;
+mkdir -p data/media2/movies;
+mkdir -p data/media2/music;
+mkdir -p data/media2/tv;
+
+mkdir -p data/torrents/complete;
+mkdir -p data/torrents/incomplete;
+mkdir -p data/torrents/downloads;
+mkdir -p data/torrents/torrent-files;
+mkdir -p data/torrents/watch;
+
+mkdir -p data/usenet/complete;
+mkdir -p data/usenet/complete/books;
+mkdir -p data/usenet/complete/movies;
+mkdir -p data/usenet/complete/music;
+mkdir -p data/usenet/complete/tv;
+
+mkdir -p data/usenet/incomplete;
 
